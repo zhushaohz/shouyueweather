6b5f6fb90b302d9ee6dfefeaa4b2ee02d3f1db9b
zhushaohz <zhushaohz@163.com>
2020-07-10 10:30:10 +0800

    Initial Commit

---
+++ shouyueweather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  shouyueweather
+//
+//  Created by zhz on 2020/7/10.
+//  Copyright © 2020 zhz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
