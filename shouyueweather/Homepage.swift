2994c8c247845f3a8ccaa95ea2c43c1cbfa01a1a
zhushaohz <zhushaohz@163.com>
2020-07-10 11:26:31 +0800

    简单做个页面

--- shouyueweather/Homepage.swift
+++ shouyueweather/Homepage.swift
@@ -10,9 +10,17 @@ import SwiftUI
 
 struct Homepage: View {
     var body: some View {
-        VStack{
+        VStack(spacing: 10) {
             Text("团结街")
-            Text("23")
+                .padding(50)
+                .font(.system(size:25))
+            Group{
+                Text("30")
+                    .font(.system(size:26))
+                Text("小雨")
+            }.padding()
+            Spacer()
+            
         }
         
     }
